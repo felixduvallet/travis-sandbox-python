21e99b24be2a235b60b6afc2adc0007d90599836
Felix Duvallet <felix.duvallet@epfl.ch>
2015-10-20 11:18:37 +0200

    another test.

--- src/test/test_double.py
+++ src/test/test_double.py
@@ -5,9 +5,14 @@ import unittest
 from double import double
 
 class TestCase(unittest.TestCase):
+
     def test_four(self):
         ret = double(4)
         self.assertEqual(8, ret)
+
+    def test_two(self):
+        ret = double(2)
+        self.assertEqual(4, ret)
 
 if __name__ == '__main__':
     unittest.main()
