4f2b10957f733044d3c7a8a6d856c949b1e5d3bb
Felix Duvallet <felix.duvallet@epfl.ch>
2015-10-21 09:13:24 +0200

    adding a bad test!

--- src/test/test_double.py
+++ src/test/test_double.py
@@ -14,5 +14,9 @@ class TestCase(unittest.TestCase):
         ret = double(2)
         self.assertEqual(4, ret)
 
+    def test_bad_math(self):
+        ret = double(3)
+        self.assertEqual(4, ret)
+
 if __name__ == '__main__':
     unittest.main()
